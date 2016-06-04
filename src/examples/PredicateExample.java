0cda50efd88efe78b66d4ff0e8c3640b6af0f576
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-06-04 14:55:23 +0300

    Using the predicate interface examples.

---
+++ src/examples/PredicateExample.java
@@ -0,0 +1,73 @@
+package examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+    public static void main(String args[]) {
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("Joe", 48));
+        people.add(new Person("John", 12));
+        people.add(new Person("Sally", 23));
+
+        //Filter the list and only display certain people.
+//        Predicate<Person> predicate = new Predicate<Person>() {
+//            @Override
+//            public boolean test(Person person) {
+//                return (person.getAge() >= 25);
+//            }
+//        };
+//
+//        for (Person person: people) {
+//            if(predicate.test(person)) {
+//                System.out.println(person.toString());
+//            }
+//        }
+
+        Predicate<Person> predicateOlder = person -> person.getAge() >= 25;
+        people.forEach(person -> {
+            if(predicateOlder.test(person)) {
+                System.out.println(person);
+            }
+        });
+    }
+
+
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
